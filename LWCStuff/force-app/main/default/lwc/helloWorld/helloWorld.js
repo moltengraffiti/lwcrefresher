6023f6ad81b88430c9cce923773443ae6c90c7e9
Sam Reilly <samreilly@gmail.com>
2021-07-16 17:06:24 +1000

    Basic Hello World with styling

--- LWCStuff/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LWCStuff/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
 
     //all client side logic in here
+    @track dynamicGreeting = "World";
+
+    greetingChangeHandler(e) {
+        this.dynamicGreeting = e.target.value;
+    }
 
 }
